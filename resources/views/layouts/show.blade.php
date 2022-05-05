415cca0bbc161d8d63f6045cc70a2ff9f4914044
Miyka96 <micaelaseminara@outlook.it>
2022-05-05 22:12:38 +0200

    fixed

--- resources/views/layouts/show.blade.php
+++ resources/views/layouts/show.blade.php
@@ -7,7 +7,7 @@
         <a class="btn btn-primary" data-toggle="collapse" href="#collapse" role="button" aria-expanded="false" aria-controls="collapse">
             Show Post Preview
         </a>
-        <div class="card " style="width:1080px;">
+        <div class="card border-0" style="width:1080px;">
             {{-- <img class="card-img-top" src="#" style="width:100%"> --}}
             <div class="card-body collapse" id="collapse">
               <h4 class="card-title text-capitalize display-5">{{$post->title}}</h4>

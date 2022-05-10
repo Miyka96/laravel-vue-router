fb857850346583b33c184f60d669ace4fd6a5230
Miyka96 <micaelaseminara@outlook.it>
2022-05-10 22:35:15 +0200

    update

--- resources/js/front.js
+++ resources/js/front.js
@@ -5,6 +5,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // VUE
 window.Vue = require('vue');
 import App from './views/App.vue'
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
 
 const app = new Vue({
     el: '#root',
